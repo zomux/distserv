990efe011546a2c4ce70a605ca7b074f6d9c020e
zomux <zomux@hotmail.com>
2013-01-13 23:37:56 +0900

    fixed hp-parser

--- applications/hp-parser.py
+++ applications/hp-parser.py
@@ -1,10 +1,10 @@
 import os, sys
 
-PATH_PREPROCESS_EN = "/home/raphael/research/preprocess-en"
-PATH_HP_PARSER = "/home/raphael/research/hp-parser"
+PREPROCESS_EN = "python /home/raphael/research/preprocess-en/preprocess-line.py"
+HP_PARSER = "python /home/raphael/research/hp-parser/hp-parser.py"
 
 def run(param):
     if 'sentence' not in param:
         return "[Error] Parameter should contain 'sentence'."
-    result = os.popen("echo \"%s\" | %s | %s" % (param['sentence'], PATH_PREPROCESS_EN, PATH_HP_PARSER)).read().strip()
+    result = os.popen("echo \"%s\" | %s | %s" % (param['sentence'], PREPROCESS_EN, HP_PARSER)).read().strip()
     return {'result':result}
